e9359af6a3de5da8bb829348232c3e9cbe192d2f
FullGreen.GN <61293366+FullGreenGN@users.noreply.github.com>
2024-08-05 15:28:12 +0200

    main

--- grid.pl
+++ grid.pl
@@ -1,16 +1,27 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
-die "program x y density" unless (scalar(@ARGV) == 3);
+
+die "Usage: $0 x y density" unless (scalar(@ARGV) == 3);
 my ($x, $y, $density) = @ARGV;
-print "$y.ox\n";
+
+# Open the file for writing
+open my $fh, '>', 'example_file' or die "Could not open file 'example_file' $!";
+
+# Print the filename as specified
+print $fh "$y.ox\n";
+
+# Generate the map and write to the file
 for (my $i = 0; $i < $y; $i++) {
     for (my $j = 0; $j < $x; $j++) {
         if (int(rand($y) * 2) < $density) {
-            print "o";
+            print $fh "o";
         } else {
-            print ".";
+            print $fh ".";
         }
     }
-    print "\n";
+    print $fh "\n";
 }
+
+# Close the file handle
+close $fh;
--- srcs/main.c
+++ srcs/main.c
@@ -3,21 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seguinyannis <seguinyannis@student.42.f    +#+  +:+       +#+        */
+/*   By: fullgreen <fullgreen@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 14:54:05 by fullgreen         #+#    #+#             */
-/*   Updated: 2024/08/05 15:16:31 by seguinyanni      ###   ########.fr       */
+/*   Updated: 2024/08/05 15:22:44 by fullgreen        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+int    ft_strlen(char *str);
 char	*file_to_string(const char *file_path);
 int	overwrite_file_with_string(const char *file_path, char *file_content);
 
 int main(int ac, char **av)
 {
+    if (ac != 2)
+    {
+        write(2, "Usage: <file_path>\n", ft_strlen("Usage: %s <file_path>\n"));
+        return (1);
+    }
+
+    
     char *str = file_to_string(av[1]);
     str[10] = 'B';
     overwrite_file_with_string(av[1], str);
