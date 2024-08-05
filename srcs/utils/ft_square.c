b4b31d07f22927aa04d6623c0386304657c89cbf
FullGreen.GN <61293366+FullGreenGN@users.noreply.github.com>
2024-08-05 16:55:35 +0200

    ft_square

--- srcs/utils/ft_square.c
+++ srcs/utils/ft_square.c
@@ -3,10 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_square.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seguinyannis <seguinyannis@student.42.f    +#+  +:+       +#+        */
+/*   By: fullgreen <fullgreen@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 16:36:27 by seguinyanni       #+#    #+#             */
-/*   Updated: 2024/08/05 16:36:28 by seguinyanni      ###   ########.fr       */
+/*   Updated: 2024/08/05 16:53:29 by fullgreen        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	**ft_split(char *str, char *charset);
+int	nb_word(char *str, char *charset);
+
+char **ft_square(char **map, int x, int y, int size)
+{
+    int i;
+    int j;
+
+    i = 0;
+    while (i < size)
+    {
+        j = 0;
+        while (j < size)
+        {
+            map[y + i][x + j] = 'x';
+            j++;
+        }
+        i++;
+    }
+    return (map);
+}
