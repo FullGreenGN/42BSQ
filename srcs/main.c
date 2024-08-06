cc1ddec8ed0bd7737647f2870b276682289eee52
yseguin <youvataque@icloud.com>
2024-08-06 12:42:05 +0200

    write on the file

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: seguinyannis <seguinyannis@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 14:54:05 by fullgreen         #+#    #+#             */
-/*   Updated: 2024/08/06 12:08:51 by seguinyanni      ###   ########.fr       */
+/*   Updated: 2024/08/06 12:41:58 by seguinyanni      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,23 @@ char			**ft_split(char *str, char *charset);
 int				overwrite_file_with_string(const char *file_path, char *file_content);
 t_args_data		string_args(char *strn, t_args_data data);
 char            **process_map(char *map);
+char			*ft_strjoin(char **map);
 
 int main(int ac, char **av)
 {
+    char    *base_str;
+	char	*end_str;
+    char	**rmap;
+
     if (ac != 2)
     {
         write(2, "Usage: <file_path>\n", ft_strlen("Usage: %s <file_path>\n"));
         return (1);
     }
-    char **map = process_map(file_to_string(av[1]));
-    for (int i = 0; map[i]; i++)
-    {
-        printf("%s\n", map[i]);
-    }
+	base_str = file_to_string(av[1]);
+    rmap = process_map(base_str);
+	end_str = malloc(ft_strlen(base_str) + 1);
+    end_str = ft_strjoin(rmap);
+	overwrite_file_with_string(av[1], end_str);
     return (ac);
 }
